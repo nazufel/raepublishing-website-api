d7224a7c8468c4ee5c2a27fe4aea896cffc2d71f
Ryan Ross <ryanalexander.ross@gmail.com>
2018-07-06 14:21:41 -0400

    working router

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	// Init router
+	r := httprouter.New()
+
+	// Add handler on /test
+	r.GET("/test", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		// Simply write some test data for now
+		fmt.Fprint(w, "Welcome!\n")
+	})
+
+	// Start the server
+	http.ListenAndServe("localhost:8080", r)
 }
