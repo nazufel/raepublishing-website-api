7b312d8ce1d16d27c19508f4a34a6bcf83a91ca2
Ryan Ross <ryanalexander.ross@gmail.com>
2018-07-30 12:56:07 -0400

    more code cleanup.

--- main.go
+++ main.go
@@ -33,21 +33,21 @@ func main() {
 	// Init UserController
 	uc := controllers.NewUserController(getSession())
 	// User Controllers
+
+	// CREATE
 	r.POST(extension+"/users/", uc.CreateUser)
 	r.PUT(extension+"/users/", uc.CreateUser)
+	// READ
 	r.GET(extension+"/users/:id", uc.GetUsers)
 	r.GET(extension+"/users/", uc.GetAllUsers)
-	/*
-		r.PATCH(extension+"/users/:id", uc.UpdateUser)
-		r.PUT(extension+"/users/:id", uc.UpdateUser)
-		r.POST(extension+"/users/:id", uc.UpdateUser)
-	*/
+	// UPDATE
 	r.PATCH(extension+"/users/firstname/:id", uc.UpdateUsersFirstname)
 	r.PATCH(extension+"/users/lastname/:id", uc.UpdateUsersLastname)
 	r.PATCH(extension+"/users/username/:id", uc.UpdateUsersUsername)
 	r.PATCH(extension+"/users/email/:id", uc.UpdateUsersEmail)
 	r.PATCH(extension+"/users/role/:id", uc.UpdateUsersRole)
 	r.PATCH(extension+"/users/bio/:id", uc.UpdateUsersBio)
+	// DELETE
 	r.DELETE(extension+"/users/:id", uc.DeleteUsers)
 	// Start the server
 	http.ListenAndServe("localhost:3000", r)
